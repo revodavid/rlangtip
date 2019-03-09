3244e28b684eba0b978843c2d3acb2df7c847622
David Smith <davidsmi@microsoft.com>
2019-03-09 14:38:41 -0600

    Update import with new .xlsx format

--- data-raw/clean_tips.R
+++ data-raw/clean_tips.R
@@ -1,9 +1,7 @@
 library(readxl)
-raw_tips <- read_excel(here::here("Daily R Language Tips for RLangTip.xlsx"))
+tips <- read_excel(here::here("Daily R Language Tips for RLangTip.xlsx"))
 
 library(dplyr)
-
-tips <- raw_tips %>% select(-X__1)
 
 readr::write_csv(tips, here::here("inst", "extdata", "tips.csv"))
 
