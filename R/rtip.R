30a488260b712896b785dc7d96512c13dc6b2e0e
David Smith <davidsmi@microsoft.com>
2019-03-09 15:28:40 -0600

    Allow printing of a specific tweet by id

--- R/rtip.R
+++ R/rtip.R
@@ -3,7 +3,6 @@
 
 ## TODO
 ## Don't import file each time -- use an object created on load
-## Allow printing of a specific tweet by id
 ## Don't show random uncategorized or deprecated tweets
 ## Allow filtering by category
 ## Word wrap tweet to width of display
@@ -12,7 +11,9 @@ rtip <- function(id) {
  ## Print a random tweet from tips.csv 
  tips <- readr::read_csv(here::here("inst", "extdata", "tips.csv"),col_types="icc?c")
  N <- NROW(tips)
- if(missing(id)) rownum <- sample(1:N,1) else {
+ if(missing(id)) { 
+  rownum <- sample(1:N,1) 
+  } else {
   rownum <- which(tips$id == id)
   if(length(rownum)==0) stop("No such tip")
  }
