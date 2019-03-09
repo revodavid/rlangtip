54205ba90fa23fa5cc8a5caeb865911e9dbfff8a
aedobbyn <adobbs11@gmail.com>
2019-03-09 14:05:33 -0500

    add save_tweet_number

--- R/get_and_score_tweets.R
+++ R/get_and_score_tweets.R
@@ -1,8 +1,18 @@
 
-get_rlangtip_tweets <- function(n = 100) {
+
+save_tweet_number <- function(val) {
+ readr::write_lines(val)
+}
+
+get_rlangtip_tweets <- function(n = 100, save_number = TRUE) {
  tbl <- rtweet::get_timeline("RLangTip", n = n)
  
- tbl <- tbl %>% 
+ if (save_number) {
+  n_tweets <- nrow(tbl)
+  save_tweet_number(n_tweets)
+ }
+ 
+ tbl %>% 
   select(status_id, created_at, text, favorite_count, retweet_count)
 }
 
@@ -14,3 +24,15 @@ score_rlangtip_tweets <- function(tbl) {
    score = favorite_count + 2*retweet_count
   )
 }
+
+get_tweet_number <- function(buffer = 100) {
+ last_val <- 
+  readr::read_lines(here::here("n_tweets.txt")) %>% 
+  as.numeric()
+ 
+ last_val + buffer
+}
+
+
+
+
