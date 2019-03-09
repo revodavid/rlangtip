36291f92fddf4eb1640b94169edb3ef97dd51660
aedobbyn <adobbs11@gmail.com>
2019-03-09 14:23:29 -0500

    update description with package imports

---
+++ R/zzz.R
@@ -0,0 +1,11 @@
+
+save_token <- function() {
+ source(here::here(twitter_keys.R))
+ 
+ create_token(
+  app = "RLangTip",
+  consumer_key = consumer_key,
+  consumer_secret = consumer_secret,
+  acess_token = acess_token,
+  access_secret = access_secret)
+}
