c4c648567f4ab0133b9f738331353d7688d39524
David Smith <davidsmi@microsoft.com>
2019-03-10 14:00:25 -0500

    prevent read_excel being run on excel

--- R/sample_tweet.R
+++ R/sample_tweet.R
@@ -1,6 +1,4 @@
 
-tweets <- read_excel("data_cleaned.xlsx")
-
 weight_determination <- function(weighted = F){
  if(weighted == F){
   return(NULL)
@@ -11,6 +9,8 @@ weight_determination <- function(weighted = F){
 
 
 sample_tweet <- function(number_of_tweets = 1, holiday = FALSE, categories = NULL, weighted = FALSE) {
+
+ tweets <- read_excel("data_cleaned.xlsx")
  
  if(length(categories) != 0){
   tweets <- tweets[(tweets$category %in% categories),]
