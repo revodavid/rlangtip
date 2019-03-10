8bec19173816ff7a6b5ab916b2775f580b259175
benjamincrary <benjamin.crary@gmail.com>
2019-03-10 12:25:55 -0500

    addressed merge conflicts

--- R/get_submissions.R
+++ R/get_submissions.R
@@ -3,11 +3,7 @@
 #' @param key Character. The key value associated with the google sheet which has the form responses.
 #' @param tab Character. Name of the google sheet tab with the form responses.
 #'
-#' @return
 #' @export
-#'
-#' @examples
-#' 
 #' 
 #' Run googlesheets:gs_ls() to initiate authentication 
 
